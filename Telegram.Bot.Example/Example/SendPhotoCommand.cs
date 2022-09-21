3a7535dfd08ca66fb410e09a1dbd1b97764f1d67
牛奶 <burpee_soggier0x@icloud.com>
2022-09-21 14:00:41 +0800

    更新：演示机器人添加新的演示方法

--- Telegram.Bot.Example/Example/SendPhotoCommand.cs
+++ Telegram.Bot.Example/Example/SendPhotoCommand.cs
@@ -15,14 +15,17 @@
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Telegram.Bot.Example.Makers;
 using Telegram.Bot.Framework;
 using Telegram.Bot.Framework.TelegramAttributes;
+using Telegram.Bot.Types;
 
 namespace Telegram.Bot.Example.Example
 {
@@ -41,6 +44,29 @@ namespace Telegram.Bot.Example.Example
             var files = Directory.GetFiles(Secrets.GetSection("iCloudPhotoPath").Value);
             await SendTextMessage("请稍后...正在发送...");
             await SendPhoto(files[Random(0, files.Length)], "这是一张随机的照片");
+        }
+
+        [Command(nameof(SendMyPhoto), CommandInfo = "向机器人发送一张图片")]
+        public async Task SendMyPhoto([Param("请发送你珍藏的图片：", true, CustomParamMaker = typeof(MyPhotoParamMaker))]PhotoSize photoSize)
+        {
+            Directory.CreateDirectory("TESTPIC");
+            using (StreamWriter sw = new StreamWriter(new FileStream($"TESTPIC/{photoSize.FileId}", FileMode.OpenOrCreate)))
+            {
+                sw.Write(JsonConvert.SerializeObject(photoSize));
+            }
+            await SendTextMessage("已经接收");
+        }
+
+        [Command(nameof(GetSendPhoto), CommandInfo = "从机器人那里随机获取一张图片")]
+        public async Task GetSendPhoto()
+        {
+            var photos = Directory.GetFiles("TESTPIC");
+
+            var file = photos[Random(0, photos.Length)];
+
+            PhotoSize onephoto = JsonConvert.DeserializeObject<PhotoSize>(System.IO.File.ReadAllText(file));
+            
+            await SendPhoto(onephoto, $"这是一张其他人传给Bot的图片，接收时间是：{new FileInfo(file).LastWriteTime}");
         }
     }
 }
