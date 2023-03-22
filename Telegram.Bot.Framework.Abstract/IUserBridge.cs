a48abfb66c4f60bcb06ec8caae0a73cdeefa50dc
牛奶 <burpee_soggier0x@icloud.com>
2023-03-22 09:30:03 +0800

    更新：修复引用

--- Telegram.Bot.Framework.Abstract/IUserBridge.cs
+++ Telegram.Bot.Framework.Abstract/IUserBridge.cs
@@ -20,6 +20,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Telegram.Bot.Framework.Abstract.Models;
 using Telegram.Bot.Types;
 
 namespace Telegram.Bot.Framework.Abstract
