08ebf3fc1d1d978c1623f6262647ff9fbb69dbdf
牛奶 <burpee_soggier0x@icloud.com>
2023-04-19 11:20:10 +0800

    更新：修改注释

--- Telegram.Bot.Framework/Attributes/BotCommandAttribute.cs
+++ Telegram.Bot.Framework/Attributes/BotCommandAttribute.cs
@@ -10,18 +10,27 @@ namespace Telegram.Bot.Framework.Attributes
     /// <summary>
     /// 机器人指令标签
     /// </summary>
+    /// <remarks>
+    /// 机器人的指令标签，将标签放置在方法上方，设置好指令之后，在Bot中输入相应的指令，即可调用相应的方法。
+    /// </remarks>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     public class BotCommandAttribute : Attribute
     {
         /// <summary>
         /// 指令，以 '/start' 形式发送的指令
         /// </summary>
+        /// <remarks>
+        /// 指令的名称，可以输入例如：`/Start`, `Start`, `start`, `START`等任意大小写，带`/`的指令。
+        /// </remarks>
         public string Command { get; } = default!;
 
         /// <summary>
         /// 指令的简单描述
         /// </summary>
-        public string Description 
+        /// <remarks>
+        /// 指令的描述，默认值为中文，`没有描述`
+        /// </remarks>
+        public string Description
         { 
             get => __Description ?? MultiLanguageStatic.Language[ItemKey.DefaultCommandDetails];
             set
@@ -34,6 +43,9 @@ namespace Telegram.Bot.Framework.Attributes
         /// <summary>
         /// 机器人指令标签
         /// </summary>
+        /// <remarks>
+        /// <paramref name="Command"/> 指令的名称，可以输入例如：`/Start`, `Start`, `start`, `START`等任意大小写，带`/`的指令。
+        /// </remarks>
         /// <param name="Command">指令，以 '/start' 形式发送的指令</param>
         public BotCommandAttribute(string Command)
         {
