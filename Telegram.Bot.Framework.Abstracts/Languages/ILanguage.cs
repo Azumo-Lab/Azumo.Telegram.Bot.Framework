f0eeb3d66e081295b22eaf500c6a5a65d0d21169
牛奶 <burpee_soggier0x@icloud.com>
2023-05-05 17:47:21 +0800

    更新：拆分接口

---
+++ Telegram.Bot.Framework.Abstracts/Languages/ILanguage.cs
@@ -0,0 +1,33 @@
+﻿//  <Telegram.Bot.Framework>
+//  Copyright (C) <2022 - 2023>  <Azumo-Lab> see <https://github.com/Azumo-Lab/Telegram.Bot.Framework/>
+//
+//  This file is part of <Telegram.Bot.Framework>: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Telegram.Bot.Framework.Abstracts.Languages
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ILanguage
+    {
+        public string LanguageName { get; }
+    }
+}
