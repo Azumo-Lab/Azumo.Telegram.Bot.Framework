359a1b10b4038f69feb544bd123a537b052f1403
牛奶 <burpee_soggier0x@icloud.com>
2023-11-22 15:44:26 +0800

    更新

---
+++ Telegram.Bot.ChannelManager/FileName.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram.Bot.ChannelManager
+{
+    internal class 角色
+    {
+        public static 角色 operator |(角色 a, string aa)
+        {
+            return new 角色();
+        }
+
+        public static 角色 operator |(角色 a, string[] aa)
+        {
+            return new 角色();
+        }
+
+        public static 角色 operator |(角色 a, IAction aa)
+        {
+            return new 角色();
+        }
+
+        public static void Test()
+        {
+            角色 角色Anzu = new();
+            角色Anzu = 角色Anzu 
+                | "笑着活下去" | ["笑.PNG", "悲情BGM.mp3"]
+                | 动作.翻转 | 等待.按秒等待(5)
+                | "再见了" | ["欢乐颂.MP3"]
+                | 动作.消失;
+        }
+    }
+
+    public class 动作
+    {
+        public static IAction 翻转 { get; }
+
+        public static IAction 消失 { get; }
+    }
+
+    public class 等待
+    {
+        public static IAction 按秒等待(int s)
+        {
+            return null;
+        }
+
+        public static IAction 按分等待(int s)
+        {
+            return null;
+        }
+    }
+
+    public interface IAction
+    {
+
+    }
+}
