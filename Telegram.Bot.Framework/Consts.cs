c837bfe2f24299c0eef793b7bb47f4c97b620258
牛奶 <burpee_soggier0x@icloud.com>
2023-12-21 14:12:57 +0800

    更新：添加常量类，添加新的扩展方法

---
+++ Telegram.Bot.Framework/Consts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram.Bot.Framework
+{
+    internal class Consts
+    {
+        /// <summary>
+        /// Bot 指令的首字符，用于区分是否是Bot 指令的字符
+        /// </summary>
+        public const char BotCommandStartChar = '/';
+    }
+}
