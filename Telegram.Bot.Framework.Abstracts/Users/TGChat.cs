9d9c82a01320d77cf55047575081da6e0f4c5ded
牛奶 <burpee_soggier0x@icloud.com>
2023-12-22 10:52:20 +0800

    更新：实现IDisposable接口

--- Telegram.Bot.Framework.Abstracts/Users/TGChat.cs
+++ Telegram.Bot.Framework.Abstracts/Users/TGChat.cs
@@ -22,7 +22,7 @@ namespace Telegram.Bot.Framework.Abstracts.Users
     /// <summary>
     /// 
     /// </summary>
-    public sealed class TGChat : Update
+    public sealed class TGChat : Update, IDisposable
     {
         /// <summary>
         /// 
@@ -53,6 +53,7 @@ namespace Telegram.Bot.Framework.Abstracts.Users
         /// 
         /// </summary>
         private readonly IServiceScope __UserServiceScope;
+        private bool disposedValue;
 
         /// <summary>
         /// 
@@ -81,6 +82,36 @@ namespace Telegram.Bot.Framework.Abstracts.Users
                 BotClient = telegramBot,
             };
             return chat;
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: 释放托管状态(托管对象)
+                    __UserServiceScope?.Dispose();
+                }
+
+                // TODO: 释放未托管的资源(未托管的对象)并重写终结器
+                // TODO: 将大型字段设置为 null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 仅当“Dispose(bool disposing)”拥有用于释放未托管资源的代码时才替代终结器
+        // ~TGChat()
+        // {
+        //     // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // 不要更改此代码。请将清理代码放入“Dispose(bool disposing)”方法中
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
