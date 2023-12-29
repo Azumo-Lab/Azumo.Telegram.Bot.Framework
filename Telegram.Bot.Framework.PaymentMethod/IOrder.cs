2ff93b50d20c0a4d53aa38f8567ae20edfc7cf34
牛奶 <burpee_soggier0x@icloud.com>
2023-12-29 11:27:34 +0800

    更新：修改部分已知问题

---
+++ Telegram.Bot.Framework.PaymentMethod/IOrder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram.Bot.Framework.PaymentMethod
+{
+    public interface IOrder
+    {
+        public void CreateOrder();
+    }
+}
