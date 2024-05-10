9991d8d1c3a799b9a4c4332f23afb1dc0a564cc2
牛奶 <burpee_soggier0x@icloud.com>
2024-05-10 11:35:35 +0800

    更新

--- Telegram.Bot.Framework/Helpers/ExceptionHelper.cs
+++ Telegram.Bot.Framework/Helpers/ExceptionHelper.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿//  <Telegram.Bot.Framework>
+//  Copyright (C) <2022 - 2024>  <Azumo-Lab> see <https://github.com/Azumo-Lab/Azumo.Telegram.Bot.Framework>
+//
+//  This file is part of <Telegram.Bot.Framework>: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
 
 namespace Telegram.Bot.Framework.Helpers
 {
+    /// <summary>
+    /// 异常帮助类
+    /// </summary>
+    /// <remarks>
+    /// 用于抛出异常，实用于各类框架
+    /// </remarks>
 #if NET8_0_OR_GREATER
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0022:使用表达式主体来表示方法", Justification = "<挂起>")]
 #endif
     internal static class ExceptionHelper
     {
+        /// <summary>
+        /// 字符串为空或null时抛出异常
+        /// </summary>
+        /// <param name="value">要检查的字符串</param>
+        /// <param name="paramName">参数名称</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void ThrowIfNullOrEmpty(string? value, string paramName)
         {
 #if NET8_0_OR_GREATER
@@ -17,6 +45,12 @@ namespace Telegram.Bot.Framework.Helpers
 #endif
         }
 
+        /// <summary>
+        /// 当参数为null时抛出异常
+        /// </summary>
+        /// <param name="value">要检查的参数</param>
+        /// <param name="paramName">参数名称</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void ThrowIfNull(object? value, string paramName)
         {
 #if NET8_0_OR_GREATER
--- Telegram.Bot.Framework/InternalCore/Users/UserMemoryCacheContextFactory.cs
+++ Telegram.Bot.Framework/InternalCore/Users/UserMemoryCacheContextFactory.cs
@@ -14,10 +14,11 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using Telegram.Bot.Framework.Core;
-using Telegram.Bot.Framework.Core.Controller;
+using Telegram.Bot.Framework.Core.Attributes;
 using Telegram.Bot.Framework.Core.Users;
 using Telegram.Bot.Framework.SimpleAuthentication;
 using Telegram.Bot.Types;
@@ -27,9 +28,21 @@ namespace Telegram.Bot.Framework.InternalCore.Users
     /// <summary>
     /// 
     /// </summary>
-    //[DependencyInjection(ServiceLifetime.Singleton, ServiceType = typeof(IContextFactory))]
-    internal class UserFilterContextFactory : BaseDictionary<long, TelegramUserContext>, IContextFactory
+    [DependencyInjection(ServiceLifetime.Singleton, ServiceType = typeof(IContextFactory))]
+    internal class UserMemoryCacheContextFactory : IContextFactory
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly IMemoryCache memoryCache;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="memoryCache"></param>
+        public UserMemoryCacheContextFactory(IMemoryCache memoryCache) =>
+            this.memoryCache = memoryCache;
+
         /// <summary>
         /// 
         /// </summary>
@@ -48,22 +61,32 @@ namespace Telegram.Bot.Framework.InternalCore.Users
             if (requestChatID == null)
                 return null;
 
-            TelegramUserContext telegramUserContext;
+            TelegramUserContext? telegramUserContext = null;
             var longChatID = requestChatID.Identifier;
             if (longChatID != null)
             {
                 var chatID = longChatID.Value;
-                if (ContainsKey(chatID))
-                    telegramUserContext = Get(chatID)!;
-                else
+                telegramUserContext = memoryCache.GetOrCreate(chatID, (cache) =>
                 {
-                    telegramUserContext = new TelegramUserContext(botServiceProvider);
-                    _ = TryAdd(chatID, telegramUserContext);
-                }
+                    var telegramUserContext = new TelegramUserContext(botServiceProvider);
+                    _ = cache.SetPriority(CacheItemPriority.NeverRemove);
+                    _ = cache.SetValue(telegramUserContext);
+                    return telegramUserContext;
+                });
             }
-            else
-                telegramUserContext = new TelegramUserContext(botServiceProvider);
+            else if (!string.IsNullOrEmpty(requestChatID.Username))
+            {
+                var username = requestChatID.Username;
+                telegramUserContext = memoryCache.GetOrCreate(username, (cache) =>
+                {
+                    var telegramUserContext = new TelegramUserContext(botServiceProvider);
+                    _ = cache.SetPriority(CacheItemPriority.NeverRemove);
+                    _ = cache.SetValue(telegramUserContext);
+                    return telegramUserContext;
+                });
+            }
 
+            telegramUserContext ??= new TelegramUserContext(botServiceProvider);
             telegramUserContext.CopyFrom(update);
             telegramUserContext.RequestChatID = requestChatID;
             return telegramUserContext;
